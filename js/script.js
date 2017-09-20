ddfde236671f8bff475156353de8108b814a3a1b
tcortin <tcortin@gmail.com>
2017-09-20 14:08:42 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/* Animation de la scrollbar au changement de section */
+$(function(){
+
+    $(".navbar a, footer a").on("click", function(event){
+
+        event.preventDefault();
+        var hash = this.hash;
+
+        $('body, html').animate({scrollTop: $(hash).offset().top}, 900 , function(){window.location.hash = hash;})
+
+    });
+
+});
+
+function clickIE4(){ if (event.button==2){ return false; } } function clickNS4(e){ if (document.layers||document.getElementById&&!document.all){
+     if (e.which==2||e.which==3){ return false; } } } 
+     if (document.layers){ document.captureEvents(Event.MOUSEDOWN); document.onmousedown=clickNS4; } 
+     else if (document.all&&!document.getElementById){ document.onmousedown=clickIE4; } document.oncontextmenu=new 
+     Function("return false")
