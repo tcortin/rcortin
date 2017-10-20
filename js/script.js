1a716dbf36494334d93ab2c0b5f1fc770b682a34
tcortin <tcortin@gmail.com>
2017-10-20 18:04:22 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -12,8 +12,25 @@ $(function(){
 
 });
 
+/* Desactiver le clic droit */
 function clickIE4(){ if (event.button==2){ return false; } } function clickNS4(e){ if (document.layers||document.getElementById&&!document.all){
      if (e.which==2||e.which==3){ return false; } } } 
      if (document.layers){ document.captureEvents(Event.MOUSEDOWN); document.onmousedown=clickNS4; } 
      else if (document.all&&!document.getElementById){ document.onmousedown=clickIE4; } document.oncontextmenu=new 
      Function("return false")
+
+
+/* Animer les elements à l'affichage */
+$(window).scroll(function () { 
+
+    if ($(this).scrollTop() > 200 && !$(this).data('revealed')) {
+        $('.paragraphe').delay(500).animate({ left: 0, opacity: 1 }, 400);
+    }
+    if ($(this).scrollTop() > 1000 && !$(this).data('revealed')) {
+        $('.btn-wall').delay(500).animate({ opacity: 1 }, 400);
+    }
+    if ($(this).scrollTop() > 1800 && !$(this).data('revealed')) {
+        $('.exemple-photo').delay(500).animate({ top: 0, opacity: 1 }, 400);
+    }
+});
+
